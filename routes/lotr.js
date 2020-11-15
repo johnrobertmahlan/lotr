627e128c00d5d4d7135d0cdcb251b3aff96dba96
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-11-15 14:27:53 -0700

    install node-schedule to run job at 9am every day

--- routes/lotr.js
+++ routes/lotr.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const axios = require('axios');
+const schedule = require('node-schedule');
 require('dotenv').config()
 const headers = { Authorization: `Bearer ${process.env.LOTRTOKEN}`}
 const phone = `${process.env.PHONE}`
@@ -7,21 +8,25 @@ const key = `${process.env.KEY}`
 let quote = '';
 let idx;
 
-axios.get('https://the-one-api.dev/v2/quote?limit=2390', { headers })
-    .then(response => {
-        idx = Math.floor(Math.random() * Math.floor(2390))
-        quote = response.data.docs[idx].dialog;
-        // axios.post('https://textbelt.com/text', {
-        //     phone: `${phone}`,
-        //     message: `${quote}`,
-        //     key: `${key}`
-        // })
-        // .then(response => {
-        //     console.log(response.data);
-        // });
-    })
-    .catch(error => {
-        console.log(error);
-    })
+schedule.scheduleJob('* 09 * * *', function() {
+    axios.get('https://the-one-api.dev/v2/quote?limit=2390', { headers })
+        .then(response => {
+            console.log("Success!");
+            idx = Math.floor(Math.random() * Math.floor(2390))
+            quote = response.data.docs[idx].dialog;
+            axios.post('https://textbelt.com/text', {
+                phone: `${phone}`,
+                message: `${quote}`,
+                key: `${key}`
+            })
+            .then(response => {
+                console.log(response.data);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
+
 
 module.exports = router;
