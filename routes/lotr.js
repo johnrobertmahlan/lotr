276944428e6e2ce44fa2d4bd171be48551bd6e7e
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-11-15 13:12:44 -0700

    add textbelt and send SMS

--- routes/lotr.js
+++ routes/lotr.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 const axios = require('axios');
 require('dotenv').config()
 const headers = { Authorization: `Bearer ${process.env.LOTRTOKEN}`}
+const phone = `${process.env.PHONE}`
 
 
 router.get('/', function(req, res) {
     res.send('<h1>LOTR!</h1>');
 });
 
-axios.get('https://the-one-api.dev/v2/movie', { headers })
+axios.get('https://the-one-api.dev/v2/quote', { headers })
     .then(response => {
         console.log("Success");
         console.log(response.data);
@@ -16,5 +17,18 @@ axios.get('https://the-one-api.dev/v2/movie', { headers })
     .catch(error => {
         console.log(error);
     })
+
+axios.post('https://textbelt.com/text', {
+        phone: `${phone}`,
+        message: 'Hello world',
+        key: 'textbelt'
+    })
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = require('./routes/lotr');
-require('dotenv').config()
 
 const app = express();
-
-// app.get('/', function(req, res) {
-//     res.send('<h1>Hello, World!</h1>');
-// });
 
 app.use('/', router);
 
