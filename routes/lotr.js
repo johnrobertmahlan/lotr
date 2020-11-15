a23d4a5fcf2f708000dc5db3ccda152fd810b29a
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-11-15 13:58:42 -0700

    increase limit of data returned by API

--- routes/lotr.js
+++ routes/lotr.js
@@ -5,20 +5,20 @@ const headers = { Authorization: `Bearer ${process.env.LOTRTOKEN}`}
 const phone = `${process.env.PHONE}`
 const key = `${process.env.KEY}`
 let quote = '';
+let idx;
 
-axios.get('https://the-one-api.dev/v2/quote', { headers })
+axios.get('https://the-one-api.dev/v2/quote?limit=2390', { headers })
     .then(response => {
-        console.log("Success");
-        quote = response.data.docs[737].dialog;
-        //use Math.random to generate a number between 1-1000, then pass that as the index to response.data.docs
-        axios.post('https://textbelt.com/text', {
-            phone: `${phone}`,
-            message: `${quote}`,
-            key: `${key}`
-        })
-        .then(response => {
-            console.log(response.data);
-        });
+        idx = Math.floor(Math.random() * Math.floor(2390))
+        quote = response.data.docs[idx].dialog;
+        // axios.post('https://textbelt.com/text', {
+        //     phone: `${phone}`,
+        //     message: `${quote}`,
+        //     key: `${key}`
+        // })
+        // .then(response => {
+        //     console.log(response.data);
+        // });
     })
     .catch(error => {
         console.log(error);
