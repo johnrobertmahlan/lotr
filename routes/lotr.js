7a7cbced605008a219baf7fd26b73a064082e4e9
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-11-15 13:38:05 -0700

    pass API data to textbelt to send quotes

--- routes/lotr.js
+++ routes/lotr.js
@@ -3,32 +3,25 @@ const axios = require('axios');
 require('dotenv').config()
 const headers = { Authorization: `Bearer ${process.env.LOTRTOKEN}`}
 const phone = `${process.env.PHONE}`
-
-
-router.get('/', function(req, res) {
-    res.send('<h1>LOTR!</h1>');
-});
+const key = `${process.env.KEY}`
+let quote = '';
 
 axios.get('https://the-one-api.dev/v2/quote', { headers })
     .then(response => {
         console.log("Success");
-        console.log(response.data);
+        quote = response.data.docs[737].dialog;
+        //use Math.random to generate a number between 1-1000, then pass that as the index to response.data.docs
+        axios.post('https://textbelt.com/text', {
+            phone: `${phone}`,
+            message: `${quote}`,
+            key: `${key}`
+        })
+        .then(response => {
+            console.log(response.data);
+        });
     })
     .catch(error => {
         console.log(error);
     })
-
-axios.post('https://textbelt.com/text', {
-        phone: `${phone}`,
-        message: 'Hello world',
-        key: 'textbelt'
-    })
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    })
-
 
 module.exports = router;
